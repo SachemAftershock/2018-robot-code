4fae69015de1529ee3bbc91ffb1d8abdf50c1cd8
Unknown <shreyasprasad14@gmail.com>
2018-01-18 20:09:50 -0500

    Cube Intake
    Controls motor's speed using A,B,X,Y on the controller

---
+++ Slipgate/src/org/usfirst/frc/team263/robot/CubeIntake.java
@@ -0,0 +1,41 @@
+package org.usfirst.frc.team263.robot;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import edu.wpi.first.wpilibj.XboxController;
+
+public class CubeIntake {
+	private static CubeIntake mInstance = new CubeIntake();
+	private TalonSRX mLeftTalon, mRightTalon;
+
+	public static CubeIntake getInstance() {
+		return mInstance;
+	}
+
+	private CubeIntake() {
+		mLeftTalon = new TalonSRX(Constants.kLeftCubeWheel);
+		mRightTalon = new TalonSRX(Constants.kRightCubeWheel);
+
+	}
+
+	public void drive(XboxController pDriver) {
+		if (pDriver.getAButtonPressed()) {
+			mRightTalon.set(ControlMode.PercentOutput, Constants.kCubeWheelSpeed / 2);
+			mLeftTalon.set(ControlMode.PercentOutput, -Constants.kCubeWheelSpeed / 2);
+			System.out.println("thing1");
+		}
+		if (pDriver.getXButtonPressed()) {
+			mRightTalon.set(ControlMode.PercentOutput, Constants.kCubeWheelSpeed);
+			mLeftTalon.set(ControlMode.PercentOutput, -Constants.kCubeWheelSpeed);
+		}
+		if (pDriver.getYButtonPressed()) {
+			mRightTalon.set(ControlMode.PercentOutput, Constants.kCubeWheelSpeed);
+			mLeftTalon.set(ControlMode.PercentOutput, -Constants.kCubeWheelSpeed);
+		}
+		if (pDriver.getBButtonPressed()) {
+			mRightTalon.set(ControlMode.PercentOutput, -Constants.kCubeWheelSpeed / 2);
+			mLeftTalon.set(ControlMode.PercentOutput, Constants.kCubeWheelSpeed / 2);
+		}
+	}
+
+}
