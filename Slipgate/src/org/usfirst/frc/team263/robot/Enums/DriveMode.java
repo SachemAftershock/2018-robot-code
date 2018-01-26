fce7c0a00cae4104c51e92be467b311d65f88d17
Rohan Bapat <rbapat@users.noreply.github.com>
2018-01-26 02:36:28 -0500

    Refactored ExperimentalDrive
    - Cleaned up and commented constants
    - Moved all enums to its own folder and solved any import errors

---
+++ Slipgate/src/org/usfirst/frc/team263/robot/Enums/DriveMode.java
@@ -0,0 +1,10 @@
+package org.usfirst.frc.team263.robot.Enums;
+
+/**
+ * Enum for closed loop drive control modes.
+ * 
+ * @author Dan Waxman
+ */
+public enum DriveMode {
+	eOpenLoop, eRotational, eLinear, eCurve, eCubeAssist;
+}
