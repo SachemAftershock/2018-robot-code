84f27046bb9dcf02b3cde296adc256abc73321fb
Dan Waxman <dan.waxman1@gmail.com>
2018-01-27 13:12:19 -0500

    Changed from DoubleSolenoid to Solenoid
    People can't make up minds sometimes... All the time... Yes.

--- Slipgate/src/org/usfirst/frc/team263/robot/Constants.java
+++ Slipgate/src/org/usfirst/frc/team263/robot/Constants.java
@@ -8,8 +8,7 @@ public class Constants {
 	final static int kRightSlaveDrivePort = 3;
 
 	// PCM Module ID
-	final static int kDriveSolenoidPortForward = 0;
-	final static int kDriveSolenoidPortReverse = 1;
+	final static int kDriveSolenoidPort = 1;
 
 	// Drivebase constants
 	final static double[] kTurningConstant = { 0.5, 0.5 };
