305d0e06c24fe088c948e965006a5293ebee9779
Dan Waxman <dan.waxman1@gmail.com>
2018-02-02 20:01:20 -0500

    Added initial JNI Implementation
    Added initial JNI implementation for profile generation. This should provide us with more memory control if generating a number of profiles during a match.

---
+++ Slipgate/src/org/usfirst/frc/team263/robot/ProfileGeneratorJNI.java
@@ -0,0 +1,5 @@
+package org.usfirst.frc.team263.robot;
+
+public class ProfileGeneratorJNI {
+	public static native double[] createNewProfile(double itp, double t1, double t2, double vprog, double dist);
+}
---
+++ Slipgate/src/org/usfirst/frc/team263/robot/native/cpp/org_usfirst_frc_team263_robot_ProfileGeneratorJNI.cpp
@@ -0,0 +1,21 @@
+#include <jni.h>
+#include "org_usfirst_frc_team263_robot_ProfileGeneratorJNI.h"
+#include "ProfileGenerator.h"
+
+JNIEXPORT jdoubleArray JNICALL Java_org_usfirst_frc_team263_robot_ProfileGeneratorJNI_createNewProfile
+(JNIEnv *env, jclass jcl, jdouble itp, jdouble t1, jdouble t2, jdouble vprog, jdouble dist)
+{
+	// Create new profile generator object and get the profile.
+	// jdouble is just a typedef of double so passing in params directly should be okay.
+	ProfileGenerator *pg = new ProfileGenerator(itp, t1, t2, vprog, dist);
+	double* profile = pg->getProfile();
+
+	// Create a new jdoublearray for the return. pg->getProfile
+	// returns an array with the length as element 0.
+	jdoubleArray profileJArray = env->NewDoubleArray(profile[0]);
+	env->SetDoubleArrayRegion(profileJArray, 0, profile[0], profile);
+
+	delete pg;
+
+	return profileJArray;
+}
